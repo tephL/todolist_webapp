b086a72d9c92a447729e0893b0b29cef2702ad51
tephL <rhezstephen06@gmail.com>
2026-02-02 14:57:26 +0800

    added function: adding a task to the DOM
    this doesnt have any kind of memory maintainability yet

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-import { addTask, showTasks, editTask, deleteTask } from './index.js';
+import { showTasks, editTask, deleteTask } from './index.js';
 
-const refresh = document.getElementById("refresh");
+const addtask_submit = document.getElementById("addtask_submit");
+const addtask_input = document.getElementById("addtask_input");
+const tasks_list = document.querySelector(".tasks_list");
 
+function addTask(value){
+    let _new = document.createElement('div');
+    _new.classList.add("task");
+    let _newP = document.createElement('p');
+    console.log(`MSG: Added ${value}`);
 
-refresh.addEventListener('click', event => {
-    console.warn("MSG: clicked refresh");
-    showTasks();    
+    _newP.textContent = value;
+    _new.appendChild(_newP);
+    tasks_list.prepend(_new);
+}
+
+// shadings when submit is being clicked
+// getting value of textbox if clicked
+addtask_submit.addEventListener("mouseover", event => {
+    event.target.style.background = 'rgba(247, 188, 209, 0.9)';
 });
 
 
-let convertedList = [];
-
-function convertDoc(){
+addtask_submit.addEventListener("click", event => {
+    event.target.style.background = 'rgba(238, 189, 206, 0.9)';
+    if(addtask_input.value != ''){
+        addTask(addtask_input.value);
+        addtask_input.value = '';
+    }else{
+        console.warn("MSG: must have input!");
+    }
     
-}
+    setTimeout(() => {
+        event.target.style.background = 'rgba(254, 189, 213, 0.9)';
+    }, 1000);
+});
 
-// stuck at transferring task objects to DOC objs
+
+addtask_submit.addEventListener("mouseout", event => {
+    event.target.style.background = 'rgba(254, 189, 213, 0.9)';
+});
